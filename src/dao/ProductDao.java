b84f0e8e580df19993645c1c26094aed36cc7a06
Namjun Kim <namjunemy@gmail.com>
2017-09-10 23:44:05 +0900

    이클립스 탭 간격 테스트

--- src/dao/ProductDao.java
+++ src/dao/ProductDao.java
@@ -10,48 +10,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDao {
-	private DataSource dataSource;
+  private DataSource dataSource;
 
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
+  public void setDataSource(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
 
-	public List<Product> selectList() throws Exception {
-		Connection connection = null;
-		Statement stmt = null;
-		ResultSet rs = null;
+  public List<Product> selectList() throws Exception {
+    Connection connection = null;
+    Statement stmt = null;
+    ResultSet rs = null;
 
-		try {
-			connection = dataSource.getConnection();
-			stmt = connection.createStatement();
-			rs = stmt.executeQuery("SELECT *" + " FROM product" + " ORDER BY p_no ASC");
-			ArrayList<Product> values = new ArrayList<>();
+    try {
+      connection = dataSource.getConnection();
+      stmt = connection.createStatement();
+      rs = stmt.executeQuery("SELECT *" + " FROM product" + " ORDER BY p_no ASC");
+      ArrayList<Product> values = new ArrayList<>();
 
-			while (rs.next()) {
-				values.add(new Product().setNo(rs.getInt("p_no")).setName(rs.getString("p_name"))
-				    .setSize(rs.getString("p_size")).setPrice(rs.getInt("p_price")).setQuantity(rs.getInt("p_quantity"))
-				    .setDescription(rs.getString("p_description")));
-			}
-			return values;
-		} catch (Exception e) {
-			throw e;
+      while (rs.next()) {
+        values.add(new Product().setNo(rs.getInt("p_no")).setName(rs.getString("p_name"))
+            .setSize(rs.getString("p_size")).setPrice(rs.getInt("p_price")).setQuantity(rs.getInt("p_quantity"))
+            .setDescription(rs.getString("p_description")));
+      }
+      return values;
+    } catch (Exception e) {
+      throw e;
 
-		} finally {
-			try {
-				if (rs != null)
-					rs.close();
-			} catch (Exception e) {
-			}
-			try {
-				if (stmt != null)
-					stmt.close();
-			} catch (Exception e) {
-			}
-			try {
-				if (connection != null)
-					connection.close();
-			} catch (Exception e) {
-			}
-		}
-	}
+    } finally {
+      try {
+        if (rs != null)
+          rs.close();
+      } catch (Exception e) {
+      }
+      try {
+        if (stmt != null)
+          stmt.close();
+      } catch (Exception e) {
+      }
+      try {
+        if (connection != null)
+          connection.close();
+      } catch (Exception e) {
+      }
+    }
+  }
 }
